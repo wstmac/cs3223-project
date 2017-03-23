41d039ce1fc315fd57e24e78311459d5f7cda13b
Fanyi <zfy926@gmail.com>
2017-03-23 12:57:58 +0800

    modify RandomOptimizer to include block nested join

--- CS3223/src/qp/optimizer/RandomOptimizer.java
+++ CS3223/src/qp/optimizer/RandomOptimizer.java
@@ -416,8 +416,11 @@ public class RandomOptimizer{
 
 	    case JoinType.BLOCKNESTED:
 
-		NestedJoin bj = new NestedJoin((Join) node);
+		BlockNested bj = new BlockNested((Join) node);
                 /* + other code */
+		bj.setLeft(left);
+		bj.setRight(right);
+		bj.setNumBuff(numbuff);
 		return bj;
 
 	    case JoinType.SORTMERGE:
