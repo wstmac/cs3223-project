d794a76306c479a6699022e6d53e49bc0bdca631
wstmac <wushuang81484@gmail.com>
2017-03-27 10:46:35 +0800

    add dynamic opt

--- CS3223/src/QueryMain.java
+++ CS3223/src/QueryMain.java
@@ -101,7 +101,7 @@ public class QueryMain{
 			System.exit(1);
 		}
 
-
+		
 
 		/** This part is used When some random initial plan is required instead of comple optimized plan **/
 		/**
@@ -120,18 +120,44 @@ public class QueryMain{
 	    execution plan
 		 **/
 
-		RandomOptimizer ro = new RandomOptimizer(sqlquery);
-		Operator logicalroot = ro.getOptimizedPlan();
-		if(logicalroot==null){
-			System.out.println("root is null");
-			System.exit(1);
+
+		Operator root = null;
+		if (numJoin > 1) {// DynamicProgrammingOptimizer
+			DynamicOptimizer dynamicOptimizer = new DynamicOptimizer(
+					sqlquery);
+			Operator logicalroot = dynamicOptimizer.getOptimalPlan();
+			if (logicalroot == null) {
+				System.out.println("root is null");
+				System.exit(1);
+			}
+			root = DynamicOptimizer.makeExecPlan(logicalroot);
+		} else {// Use random Optimization algorithm to get a random optimized
+				// execution plan
+			RandomOptimizer ro = new RandomOptimizer(sqlquery);
+			Operator logicalroot = ro.getOptimizedPlan();
+			if (logicalroot == null) {
+				System.out.println("root is null");
+				System.exit(1);
+			}
+			root = RandomOptimizer.makeExecPlan(logicalroot);
 		}
 
 
-
-		/** preparing the execution plan **/
-
-		Operator root = RandomOptimizer.makeExecPlan(logicalroot);
+//		RandomOptimizer ro = new RandomOptimizer(sqlquery);
+//		Operator logicalroot = ro.getOptimizedPlan();
+//		if(logicalroot==null){
+//			System.out.println("root is null");
+//			System.exit(1);
+//		}
+//
+//
+//
+//		/** preparing the execution plan **/
+//
+//		Operator root = RandomOptimizer.makeExecPlan(logicalroot);
+		
+		//===================================================================//
+		
 
 		/** Print final Plan **/
 		System.out.println("----------------------Execution Plan----------------");
